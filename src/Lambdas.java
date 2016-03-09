739bbb3c31a139c1b2bca8fef8350890d547df33
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-03-09 16:21:59 +0000

    added Q2

--- src/Lambdas.java
+++ src/Lambdas.java
@@ -1,9 +1,21 @@
 import java.util.Arrays;
 
-public class Outline {
+public class Lambdas {
+
+    public static int compare(String s1, String s2) {
+        int compareFlag = 0;
+        if (s1.contains("e") && !s2.contains("e")) {
+            compareFlag = -1;
+        } else if (s2.contains("e") && !s1.contains("e")) {
+            compareFlag = 1;
+        }
+        return compareFlag;
+    }
+
     public static void main(String... args) { // varargs alternative to String[]
         String[] stringArray = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
-        System.out.println(Arrays.asList(stringArray));
+        System.out.println("***** Question 1 *****");
+        System.out.println("Original list: " + Arrays.asList(stringArray));
         Arrays.sort(stringArray, (s1, s2) -> s1.length() - s2.length());
         System.out.println("In order of length: " + Arrays.asList(stringArray));
         Arrays.sort(stringArray, (s1, s2) -> s2.length() - s1.length());
@@ -20,5 +32,13 @@ public class Outline {
             return (compareFlag);
         });
         System.out.println("All that contain \"e\" first: " + Arrays.asList(stringArray));
+
+        System.out.println("\n***** Question 2 *****");
+        Arrays.sort(stringArray, (s1, s2) -> s1.length() - s2.length());
+        System.out.println("Original list: " + Arrays.asList(stringArray));
+        Arrays.sort(stringArray, (Lambdas::compare));
+        System.out.println("All that contain \"e\" first using method reference: " + Arrays.asList(stringArray));
+
+
     }
 }
